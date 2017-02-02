d5b7ae27390d4854cb1648c7c66276d671cf72bb
game110pm <game110pm@605-110-STU08>
2017-02-02 12:51:02 -0700

    2/2/17

---
+++ What song ya want/What song ya want/What song ya want.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int input;
+	cout << "Do you want to listen to Eminem, Bruno Mars, or Rick Ashley?" << endl;
+	cin >> input;
+	if (input == 1)
+		cout << "And if you ask me too Daady's gonna buy you a mocking bird, I'mma give you the world. I'mma buy a diamond ring for you, I'mma sing for you, I'll do anything to see you smile..." << endl;
+	if (input == 2)
+		cout << "But darling I'll still catch a grenade for ya! Throw my head on a plate for ya! I'll jump in front of a train for ya! You know I'll do anything for ya...." << endl;
+	if (input == 3)
+		cout << "Never gonna give you up, never gonna let you down, never turn around, desert you! Never gonna make you cry, never gonna SAY goodbye, never gonna tell a lie, and hurt you...." << endl;
+}
