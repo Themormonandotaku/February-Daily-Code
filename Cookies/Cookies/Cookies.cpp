ac5db7920f9b12bdce9eec4ba5ce89e5d2186722
game110pm <game110pm@605-110-STU08>
2017-02-06 13:05:02 -0700

    2/6/17

--- Cookies/Cookies/Cookies.cpp
+++ Cookies/Cookies/Cookies.cpp
@@ -4,9 +4,9 @@ int main() {
 	int cookies;
 	cout << "Ruby, how many cookies do you want this time?" << endl;
 	cin >> cookies;
-	if (cookies < 5)
+	if (cookies <= 5)
 		cout << "You sure you want that litte? Alright, whatever." << endl;
-	else if (cookies < 10)
+	else if (cookies <= 10)
 		cout << "Okay, here." << endl;
 	else if (cookies > 10)
 		cout << "Ruby, do we need to have the talk about diabetes again?" << endl;
